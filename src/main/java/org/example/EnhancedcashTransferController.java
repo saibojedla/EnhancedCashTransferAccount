9e4f592128140a06336cef7ac51c4c4975a17298
SAI <bojedlasaiprasad@gmail.com>
2025-05-18 17:40:35 -0400

    Creating enhanced cash tranfers with user profile and client poid

---
+++ src/main/java/org/example/EnhancedcashTransferController.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class EnhancedcashTransferController {
+    public static void main(String[] args) {
+    System.out.println("Enhancedcashtransfers");
+    System.out.println("Userprofile");
+    System.out.println("Clientpoid");
+    }
+
+}
